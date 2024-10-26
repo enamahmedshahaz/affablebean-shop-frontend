5d2c8e9def0388c27b547ab0a5eb6f5a808aaaca
enamahmedshahaz <enamahmedshahaz@gmail.com>
2024-10-26 15:45:27 +0600

    add firebase config

---
+++ src/pages/Home/Categories.jsx
@@ -0,0 +1,11 @@
+
+
+const Categories = () => {
+    return (
+        <div>
+            <h2 className="text-3xl font-bold uppercase text-green-700">Shop by Categories</h2>
+        </div>
+    );
+};
+
+export default Categories;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import Banner from "./Banner";
+import Categories from "./Categories";
 
 const Home = () => {
     return (
         <div>
             <div className="my-6">
                 <Banner></Banner>
+            </div>
+            <div>
+                <Categories></Categories>
             </div>
         </div>
     );
